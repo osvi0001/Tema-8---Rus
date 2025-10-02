ac706927355949e5861442b8231155a0bf3f9c9a
osvi0001 <osvi0001@stud.kea.dk>
2025-10-02 09:35:16 +0200

    sorted

--- js/productlist.js
+++ js/productlist.js
@@ -7,8 +7,48 @@ document
   .querySelectorAll(".filters button")
   .forEach((knap) => knap.addEventListener("click", showFiltered));
 
-function showFiltered() {
-  const gender = this.dataset.gender;
+document
+  .querySelectorAll(".sortering button")
+  .forEach((knap) => knap.addEventListener("click", showSorted));
+
+document.querySelectorAll(".filters button").forEach((knap) =>
+  knap.addEventListener("click", function (event) {
+    // fjern active fra alle filter-knapper
+    document
+      .querySelectorAll(".filters button")
+      .forEach((btn) => btn.classList.remove("active"));
+    // tilføj active til den valgte
+    event.target.classList.add("active");
+    // kør din funktion
+    showFiltered(event);
+  })
+);
+
+document.querySelectorAll(".sortering button").forEach((knap) =>
+  knap.addEventListener("click", function (event) {
+    // fjern active fra alle sorterings-knapper
+    document
+      .querySelectorAll(".sortering button")
+      .forEach((btn) => btn.classList.remove("active"));
+    // tilføj active til den valgte
+    event.target.classList.add("active");
+    // kør din funktion
+    showSorted(event);
+  })
+);
+
+function showSorted(event) {
+  const direction = event.target.dataset.direction;
+  if (direction === "lohi") {
+    allData.sort((a, b) => a.price - b.price);
+  } else {
+    allData.sort((a, b) => b.price - a.price);
+  }
+  showProducts(allData);
+}
+
+function showFiltered(event) {
+  const gender = event.target.dataset.gender;
   if (gender == "All") {
     showProducts(allData);
   } else {
